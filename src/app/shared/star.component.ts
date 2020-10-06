9fd4b859c05ca4b6d95dcc2adeeeaa19e223af4b
camilarodrigues <camilarodrigues@squaretrade.com>
2020-10-06 09:33:07 -0300

    Angular Getting Started by Debora Kurata from Pluralsight

---
+++ src/app/shared/star.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core'
+
+@Component({
+  selector: 'pm-star',
+  templateUrl: './star.component.html',
+  styleUrls: ['./star.component.scss']
+})
+export class StarComponent implements OnChanges {
+  @Input() rating: number;
+  @Output() ratingClicked: EventEmitter<String> = new EventEmitter<String>();
+  starWidth: number;
+
+  ngOnChanges(): void {
+    this.starWidth = this.rating * 75 / 5;
+  }
+
+  onClick(): void {
+    this.ratingClicked.emit(`The rating ${this.rating} was clicked!`);
+  }
+}
